4e1c7acb4cf0b13bb9c04a42cd97cd4db2b1b8fb
GuDongyeong <ehd1722@naver.com>
2020-08-19 21:45:14 +0900

    구동영 : blacklist 진행 중

--- src/main/java/com/privateplaylist/www/admin/board/controller/BlackListController.java
+++ src/main/java/com/privateplaylist/www/admin/board/controller/BlackListController.java
@@ -1,5 +1,8 @@
 package com.privateplaylist.www.admin.board.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +22,19 @@ public class BlackListController {
 		
 		ModelAndView mav = new ModelAndView();
 		
+		List<Map<String, Object>> list = blackListService.selectAllBlackList();
+		mav.addObject("list", list);
+		mav.setViewName("admin/blackList");
+		return mav;
+	}
+	
+	@RequestMapping("/blacklist/turndown")
+	public ModelAndView turndown() {
 		
+		ModelAndView mav = new ModelAndView();
 		
-		
-		
+		List<Map<String, Object>> list = blackListService.selectAllBlackList();
+		mav.addObject("list", list);
 		mav.setViewName("admin/blackList");
 		return mav;
 	}
