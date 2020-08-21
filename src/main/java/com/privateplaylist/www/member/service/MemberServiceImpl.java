7bb2449602828fc8edf3c44b476a47b82b7fc25c
Kim heonil <iamheonil@gmail.com>
2020-08-21 20:46:46 +0900

    김헌일 : 아이디 중복검사, 비밀번호 정규화식 삽입, 이메일 추가 작업 중

--- src/main/java/com/privateplaylist/www/member/service/MemberServiceImpl.java
+++ src/main/java/com/privateplaylist/www/member/service/MemberServiceImpl.java
@@ -2,11 +2,14 @@ package com.privateplaylist.www.member.service;
 
 import com.privateplaylist.www.member.dao.MemberDao;
 import com.privateplaylist.www.member.vo.Member;
+import common.util.FileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -15,25 +18,43 @@ public class MemberServiceImpl implements MemberService {
     @Autowired
     MemberDao memberDao;
 
-    @Autowired
-    JavaMailSender mailSender;
-
 //    @Autowired
-//    BCryptPasswordEncoder passwordEncoder;
+//    JavaMailSender mailSender;
+
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
 
     @Override
     public int insertMember(Member member) {
 
-//      String password = member.getUserPw();
+      String password = member.getUserPw();
 
-//      password = passwordEncoder.encode(password);
+      password = passwordEncoder.encode(password);
 
-        // member.setUserPw(password);
+      member.setUserPw(password);
 
-        System.out.println("Join Method : " + member);
+      System.out.println("Join Method : " + member);
 
-        return memberDao.insertMember(member);
+      // int res = memberDao.insertMember(member);
+
+      return memberDao.insertMember(member);
     }
+
+//    @Override
+//    public int insertTeacherFile(Member member, List<MultipartFile> files, String root) {
+//
+//        int result = memberDao.insertFile(member);
+//
+//        if(!(files.size() == 1 && files.get(0).getOriginalFilename().equals(""))) {
+//
+//            List<Map<String, String>> filedata = new FileUtil().fileUpload(files, root);
+//
+//            for (Map <String, String> f : filedata) {
+//                memberDao.insertFile(f);
+//            }
+//        }
+//        return result;
+//    }
 
     @Override
     public Member selectMember(Map<String, Object> memberMap) {
@@ -41,7 +62,8 @@ public class MemberServiceImpl implements MemberService {
     }
 
     @Override
-    public void mailSending(Member member, String urlPath) {
-
+    public int selectId(String userId) {
+        return memberDao.selectId(userId);
     }
+
 }
