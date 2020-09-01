b8edbe49dd28f93bb6ca72211e08a3b7f15f1143
GuDongyeong <62394108+GuDongyeong@users.noreply.github.com>
2020-09-01 21:27:25 +0900

    구동영 : 학생페이지 중고장터 리스트 불러오기 (#264)
    * 구동영 : push
    
    * 구동영 : blacklist 작업 중
    
    * 구동영 : pr
    
    * m
    
    * 구동영 : push
    
    * commit
    
    * m
    
    * 구동영 : blacklist 작업중
    
    * 구동영 : commit
    
    * 구동영 : blacklist
    
    * 구동영 : pr
    
    * 구동영 : pull
    
    * 구동영 : commit
    
    * 구동영 : pr
    
    * 구동영 : pr
    
    * m
    
    * m
    
    * 구동영 : pr
    
    * m
    
    * 구동영 : 문제되는 bean 파일지움
    
    * 구동영 : teacher 학생 신청 내역
    
    * m
    
    * 구동영 : 선생님 페이지 학생 신청 관리
    
    * m
    
    * m
    
    * m
    
    * m
    
    * 구동영 : 선생님 페이지 학생 관리 내역 완료
    
    * 구동영 : blacklist.jsp 수정
    
    * 구동영 : 선생님 페이지 학생신청내역 마무리
    
    * 구동영 : 선생님 페이지
    
    * m
    
    * 구동영 : 선생님페이지 연결된 과외 - 학생정보, 과외종료
    
    * m
    
    * 구동영 : 학생 페이지 중고장터- 전체 리스트불러오기
    
    Co-authored-by: KIM HEONIL <iamheonil@gmail.com>

--- src/main/java/com/privateplaylist/www/student/market/controller/marketStuController.java
+++ src/main/java/com/privateplaylist/www/student/market/controller/marketStuController.java
@@ -1,22 +1,47 @@
 package com.privateplaylist.www.student.market.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.privateplaylist.www.member.vo.Member;
+import com.privateplaylist.www.student.market.service.MarketStuService;
+
+import common.util.Paging;
+
 @Controller
 @RequestMapping("/student")
 public class marketStuController {
+	
+	@Autowired
+	private MarketStuService marketStuService;
 
 	@RequestMapping(value = "/market", method = RequestMethod.GET)
 	public ModelAndView signStudent(HttpSession session, @RequestParam(required = false, defaultValue = "1") int curPage) {
 		
 		ModelAndView mav = new ModelAndView();
 		
+		Member m = (Member) session.getAttribute("loginUser");
+		
+		int userNo = 6;
+		
+		Paging paging = marketStuService.getPagingMarketStu(curPage, userNo);
+		
+		List<Map<String, Object>> list = marketStuService.selectMarketStu(paging, userNo);
+		
+		System.out.println(list);
+		
+		mav.addObject("paging", paging);
+		mav.addObject("list", list);
+		mav.setViewName("teacher/connectedLesson/signStudent");
 		mav.setViewName("student/market/marketlist");
 		
 		return mav;
