b28088bc2c912bf5ed532b4af53ef682e7f9d751
KimSuHyeon <tngus2280@naver.com>
2020-09-02 22:18:30 +0900

    김수현 : 선생님 마이페이지 질문게시판 작성중

--- src/main/java/com/privateplaylist/www/teacher/board/controller/TeacherReviewController.java
+++ src/main/java/com/privateplaylist/www/teacher/board/controller/TeacherReviewController.java
@@ -1,6 +1,8 @@
 package com.privateplaylist.www.teacher.board.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -8,7 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.privateplaylist.www.dto.Question;
 import com.privateplaylist.www.dto.Review;
 import com.privateplaylist.www.teacher.board.service.TeacherReviewService;
 
@@ -42,4 +47,43 @@ public class TeacherReviewController {
 				return "teacher/board/review/list";
 		
 			}
+			
+			
+//			//검색
+//			@RequestMapping(value="/search",method = RequestMethod.POST )
+//			public String questionSearch(Model model,@RequestParam String keyword,HttpServletRequest req) {
+//				
+//				//root context
+//				String root = req.getContextPath();
+//				
+//				//요청 파라미터를 전달하여 paging 객체 생성하기
+//				Paging paging = teacherQuestionService.questionSearchPaging(req,keyword);
+//				
+//				//키워드와 페이징을 넣어줄 map
+//				Map<String, Object> searchMap = new HashMap<String, Object>();
+//				
+//				searchMap.put("keyword", keyword);
+//				searchMap.put("paging", paging);
+//				//글 검색하기
+//				List<Question> questionSearchList = teacherQuestionService.selectSearchQuestion(searchMap);
+//				
+//				if(keyword.equals("")) {
+//					model.addAttribute("alertMsg", "검색어를 입력해주세요");
+//					model.addAttribute("url", root+"/teacher/question/list");
+//
+//					return "/teacher/question/error";
+//					
+//				}
+//				
+//				//모델값 전달
+//				model.addAttribute("keyword", keyword);
+//				model.addAttribute("questionSearchList", questionSearchList);
+//				
+//				//페이징 결과 전달
+//				model.addAttribute("paging", paging);
+//				
+//				
+//				//검색 완료
+//				return "/teacher/question/search";
+//			}
 }
--- src/main/webapp/WEB-INF/views/teacher/board/question/detail.jsp
+++ src/main/webapp/WEB-INF/views/teacher/board/question/detail.jsp
@@ -17,5 +17,11 @@
 
 
 
+<div id="divbtn">
+		<a href="${pageContext.request.contextPath}/teacher/question/list" class="anone"><button type="button" class="btn btn-default" id="listbtn">목록</button></a>
+<button type="button" class="button button2" id="updatebtn">수정</button>
+<button type="button" class="button button3" id="deletebtn">삭제</button>
+	</div> 
+
 <c:import url="/WEB-INF/layout/teacher/teaFooter.jsp"></c:import>
 <c:import url="/WEB-INF/layout/teacher/teaFooter2.jsp"></c:import>
