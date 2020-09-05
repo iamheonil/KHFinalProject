5efa1de33ca6e057ba794167ca1224bfac75cbc4
lhg0314 <57593975+lhg0314@users.noreply.github.com>
2020-09-05 23:11:09 +0900

    이효진: 과외찾기jsp,페이징 마무리 (#301)
    * 채팅창 팝업창열리도록 수정
    
    * 학생 웹쉐어 commit
    
    * 이효진: 수강중인 과외,자료실 commit
    
    * 이효진: commit for pull
    
    * commit for pulll
    
    * 이효진:main header에 url 추가
    
    Co-authored-by: hyojin <hi8087470@gmail.com>
    Co-authored-by: KIM HEONIL <iamheonil@gmail.com>

--- src/main/java/com/privateplaylist/www/member/dao/FindLessonDao.java
+++ src/main/java/com/privateplaylist/www/member/dao/FindLessonDao.java
@@ -13,15 +13,22 @@ public class FindLessonDao {
 	@Autowired
 	SqlSessionTemplate sqlSession;
 
+	//페이징을 위한 게시물의 수 반환
 	public int selectTotalCnt(Map<String, Object> option) {
 		// TODO Auto-generated method stub
 		return sqlSession.selectOne("MemberFindLesson.selectTotalCnt",option);
 	}
 
+	//페이징 처리를 한 게시물 가져오기
 	public List<Map<String, Object>> selectByOption(Map<String, Object> optionMap) {
 		// TODO Auto-generated method stub
 		return sqlSession.selectList("MemberFindLesson.selectByOption",optionMap);
 	}
+	
+	public List<Map<String,Object>> selectStarAndReview(){
+		return sqlSession.selectList("MemberFindLesson.selectStarAndReview");
+	}
+	
 
 
 
