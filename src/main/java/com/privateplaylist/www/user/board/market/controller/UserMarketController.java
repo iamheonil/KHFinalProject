13b78e931a80ded94551f03e8d6f75235dd4c55b
inju521 <lyj97521@naver.com>
2020-09-08 18:31:41 +0900

    이인주 : 선생님 과외 작성 mapper 수정

--- src/main/java/com/privateplaylist/www/user/board/market/controller/UserMarketController.java
+++ src/main/java/com/privateplaylist/www/user/board/market/controller/UserMarketController.java
@@ -19,7 +19,7 @@ import common.util.Paging;
 
 @Controller
 @RequestMapping("board")
-public class UserMarketController {
+public class userMarketController {
 	
 	@Autowired
 	private UserMarketService userMarketService;
